29093db527175624ef8dd75f869dabb924e8b6ee
B V R Srinivasa Subhash <53014779+SriT333@users.noreply.github.com>
2019-08-09 12:15:38 +0530

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,11 +1,12 @@
 #include<iostream.h>
+using namespace std; 
 int main(){
   int x,p;
   float next_amt,amount_ness;
   cin>>x>>p;
   next_amt=x;
   amount_ness=0;
-  while(next_amt>1){
+  while(next_amt>=1){
     amount_ness+=next_amt;
     next_amt=x-(p*x)/100;
     }
