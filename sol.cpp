b729d9f3c8bd65983a4cdf4db08aa0833dabd720
B V R Srinivasa Subhash <53014779+SriT333@users.noreply.github.com>
2019-08-09 11:57:35 +0530

    sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+int main(){
+  int x,p;
+  float next_amt,amount_ness;
+  cin<<x<<p;
+  next_amt=x;
+  amount_ness=0;
+  while(nrxt_amt>1){
+    amount_ness+=next_amt;
+    next_amt=x-(p*x)/100;
+    }
+  cout<<amount_ness<<"\n";
+  return 0;
+}
+    
+    
